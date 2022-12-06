1a955a5c84080283d2a863e30cfac2d8ff3e0b1c
Carlos Rodriguez <carlos.rodriguez@100ladrillos.com>
2022-12-06 08:13:46 -0600

    (#10): Create list view of projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Content, Header } from '@/components';
-import { CreateProject } from '@/views';
+import { CreateProject, ProjectsList } from '@/views';
 
 const App = () => (
   <Router>
     <Header />
     <Content>
       <Routes>
-        <Route path="/projects" element={<div><p>Projects List</p></div>} />
+        <Route path="/projects" element={<ProjectsList />} />
         <Route path="/projects/create" element={<CreateProject />} />
       </Routes>
     </Content>
--- client/src/hooks/useClient/ProjectsClient.js
+++ client/src/hooks/useClient/ProjectsClient.js
@@ -1,6 +1,10 @@
 import HttpClient from './httpClient';
 
 class ProjectsClient extends HttpClient {
+  async getList() {
+    return this._instance.get('/');
+  }
+
   async create(name, description) {
     return this._instance.post('/', { name, description });
   }

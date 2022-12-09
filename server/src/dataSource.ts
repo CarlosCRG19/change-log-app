55f7920c7056e556fd2a1aeef09d684acc3db4f1
CarlosCRG19 <carloscrg19@gmail.com>
2022-12-09 09:27:13 -0600

    (#25): Deploy server on Heroku

--- server/src/dataSource.ts
+++ server/src/dataSource.ts
@@ -2,16 +2,36 @@ import { DataSource } from 'typeorm';
 
 import * as models from '@/models';
 
-const dataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: Object.values(models),
-  synchronize: true,
-  logging: false,
-});
+const createDataSource = (): DataSource => {
+  const {
+    DB_URL,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_DATABASE,
+  } = process.env;
+
+  if (DB_URL !== undefined && typeof DB_URL === 'string') {
+    return new DataSource({
+      type: 'postgres',
+      url: DB_URL,
+      entities: Object.values(models),
+      extra: { rejectUnauthorized: false },
+    });
+  }
+
+  return new DataSource({
+    type: 'postgres',
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    entities: Object.values(models),
+  });
+};
+
+const dataSource = createDataSource();
 
 export default dataSource;
